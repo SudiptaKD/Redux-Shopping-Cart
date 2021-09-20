7aad22baf7b33846ce99f54ab781d85f10267244
Sudipta Kumar Dam <sudiptakumardam@gmail.com>
2021-09-20 13:33:59 +0600

    mobile responsive

--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -9,7 +9,7 @@ import Product from "./Product/Product";
 const Products = ({ products }) => {
   return (
     < >
-      <Row lg={3} md={2} sm={1} xs={1}>
+      <Row xxl={4} lg={3} md={2} sm={1} xs={1}>
         {products.map((product) => (
           <Col key={product.id}  >
             <Product  product={product} />
